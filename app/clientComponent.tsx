4731911e3b6009b6896e245926c68c20bf32eb63
agusrog <agustin.arg1@gmail.com>
2023-08-03 16:24:27 -0300

    use client

---
+++ app/clientComponent.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { Web3ReactProvider } from "@web3-react/core"
+import { Providers } from "./providers"
+import Navbar from "@/components/Navbar"
+import { getLibrary } from "@/config/web3"
+
+ 
+export default function ClientComponent({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <Providers>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Navbar />
+      {children}
+    </Web3ReactProvider>
+  </Providers>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
-'use client'
+
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Providers } from './providers';
-import { Web3ReactProvider } from '@web3-react/core';
-import { getLibrary } from '@/config/web3';
-import Navbar from '@/components/Navbar';
+import ClientComponent from './clientComponent';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,12 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-          <Web3ReactProvider getLibrary={getLibrary}>
-            <Navbar />
-            {children}
-          </Web3ReactProvider>
-        </Providers>
+        <ClientComponent>
+          {children}
+        </ClientComponent>
       </body>
     </html>
   )
